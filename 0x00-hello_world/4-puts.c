66dcb5255deed7cd73a61d6c3582d05f4e10c3c8
Raid55 <boutboulnicholas@gmail.com>
2017-09-13 20:15:43 +0000

    names and puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(void){
-    puts("\"Programming is like building a multilingual puzzle\n");
+    puts("\"Programming is like building a multilingual puzzle");
+    puts;
     return 0;
 }
