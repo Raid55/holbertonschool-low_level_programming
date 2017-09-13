1758b60e7fcabb419186d68b72de6190e6e07e2b
Raid55 <boutboulnicholas@gmail.com>
2017-09-13 21:12:08 +0000

    gotta have they stylish betty!, thanks for the style script!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - My first hello world c programm
+ *
+ * Description: Pretty self explanatory
+ * This is just hello world with puts
+ * Return: 0
+ */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
