87ed150a20ee71cbbebac6af6759bcd328a3ef49
Raid55 <boutboulnicholas@gmail.com>
2017-09-13 21:27:00 +0000

    hello world with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - My second hello world c programm
+ *
+ * Description: Pretty self explanatory
+ * This is just hello world with printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
