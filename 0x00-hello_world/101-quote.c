bc8685439843e80fb279d29f86f0c0ca6b5f1d6e
Raid55 <boutboulnicholas@gmail.com>
2017-09-13 22:44:45 +0000

    101 quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Outputs to stderr
+ *
+ * Description: this function uses fprintf
+ * to print to certain output, stderr
+ * Return: 1
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
