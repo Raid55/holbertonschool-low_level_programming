71e655e5c05609612ea22ae7f7fa190a0d46eb2b
Raid55 <boutboulnicholas@gmail.com>
2017-09-13 21:58:18 +0000

    forgot a and an

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,10 +17,10 @@ int main(int argc, char *argv[])
 	long long int longLongInteger;
 	float iceCreamFloat;
 
-	printf("Size of char: %ld byte(s)\n", sizeof(character));
-	printf("Size of int: %ld byte(s)\n", sizeof(integer));
-	printf("Size of long int: %ld byte(s)\n", sizeof(longInteger));
-	printf("Size of long long int: %ld byte(s)\n", sizeof(longLongInteger));
-	printf("Size of float: %ld byte(s)\n", sizeof(iceCreamFloat));
+	printf("Size of a char: %ld byte(s)\n", sizeof(character));
+	printf("Size of an int: %ld byte(s)\n", sizeof(integer));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longInteger));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longLongInteger));
+	printf("Size of a float: %ld byte(s)\n", sizeof(iceCreamFloat));
 	return (0);
 }
