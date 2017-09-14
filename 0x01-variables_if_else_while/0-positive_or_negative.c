f780a16fc112cd2f7e83f2e75db854c570567b05
Raid55 <boutboulnicholas@gmail.com>
2017-09-14 17:13:42 +0000

    boiler plate, first ex of the day

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Outputs to stderr
+ *
+ * Description: this function uses fprintf
+ * to print to certain output, stderr
+ * Return: 1
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else if (n > 0)
+		printf("%d is positive\n", n);
+
+	return (0);
+}
