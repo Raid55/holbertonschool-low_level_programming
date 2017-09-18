986e5203af00153d283c67a5f3f9dbba001e5902
Raid55 <boutboulnicholas@gmail.com>
2017-09-18 19:37:31 +0000

    boilerplate and initial test

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "_putchar.c"
+
+/**
+ * main - prints holberton
+ *
+ * Description: does things
+ * Returns: 0
+ */
+int main(void){
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ayoo, this is c reloaded without all of that useless, time wasting, no good of a library, stdio.h
