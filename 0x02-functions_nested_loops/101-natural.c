1efdc64f8c7fa8a98a3cd028865f06bc7d4ed2ae
Raid55 <boutboulnicholas@gmail.com>
2017-09-19 20:37:59 +0000

    natural with main only?

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-#include "holberton.h"
 
 /**
- * natural - prints natural numbs
+ * main - prints natural numbs
  *
  * Description: prints natural nums
  * in a cool way
  * Return: void
  */
-void natural(void)
+int main(void)
 {
 	int accu = 0, i = 0;
+
 	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			accu += i;
 	}
 	printf("%d\n", accu);
-
+	return (0);
 }
