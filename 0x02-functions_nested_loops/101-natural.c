9ecf116a4c91405c4e993b2f3ad18cffeef7fd97
Raid55 <boutboulnicholas@gmail.com>
2017-09-19 20:30:46 +0000

    Naturel...oui...naturel..oui, natruel naturel naturel, OUI

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,17 +10,11 @@
  */
 void natural(void)
 {
-	int accu = 0, i = 0 ,tmp = 0, tmp2 = 0;
-
-	while (tmp < 1024 || tmp2 < 1024)
+	int accu = 0, i = 0;
+	for (i = 0; i < 1024; i++)
 	{
-		tmp = 3 * i;
-		tmp2 = 5 * i;
-		if (tmp == tmp2)
-			accu += tmp;
-		else
-			accu += tmp + tmp2;
-		i++;
+		if (i % 3 == 0 || i % 5 == 0)
+			accu += i;
 	}
 	printf("%d\n", accu);
 
