c7627b6950af783485e658b74fdad1ef8bd5b7f3
Raid55 <boutboulnicholas@gmail.com>
2017-09-19 19:35:33 +0000

    its a sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,10 +11,18 @@ int print_sign(int i)
 {
 
 	if (i < 0)
-		return (-3);
+	{
+		_putchar('-');
+		return (-1);
+	}
 	else if (i > 0)
-		return (-5);
+	{
+		_putchar('+');
+		return (1);
+	}
 	else
+	{
+		_putchar('0');
 		return (0);
-
+	}
 }
