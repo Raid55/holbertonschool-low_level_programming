ba69ff1d449209abe8f551df2d6bcd9472816deb
Raid55 <boutboulnicholas@gmail.com>
2017-09-20 03:10:21 +0000

    103 fib test

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints sum of only evens fib
+ *
+ * Description: prints sum of even  fib numbers up to 4mill
+ * in a cool way
+ * Return: 0
+ */
+int main(void)
+{
+	long fib = 1, fib2 = 2, fib3 = 0;
+	long sum = 0;
+
+	while (fib <= 4000000)
+	{
+		fib3 = fib + fib2;
+		fib = fib2;
+		fib2 = fib3;
+		if (fib3 % 2 == 0)
+			sum += fib3;
+		else
+			continue;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
