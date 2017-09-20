0820191c1389b2e3698a70af3cb874b8a676345e
Raid55 <boutboulnicholas@gmail.com>
2017-09-20 02:37:12 +0000

    galaleo...fibonacci...aristotle...freud...nitche...etc

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints fib numbers to 50
+ *
+ * Description: prints fib numbers
+ * in a cool way
+ * Return: 0
+ */
+int main(void)
+{
+	long fib = 1, fib2 = 2, fib3 = 0;
+	int i = 2;
+
+	printf("%ld, %ld, ", fib, fib2);
+	while (i < 49)
+	{
+		fib3 = fib + fib2;
+		if (i == 48)
+			printf("%ld\n", fib3);
+		else
+			printf("%ld, ", fib3);
+		fib = fib2;
+		fib2 = fib3;
+		i++;
+	}
+	return (0);
+}
