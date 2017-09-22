e500def4f4e757765d785abe865aadba29d06aae
Raid55 <boutboulnicholas@gmail.com>
2017-09-22 02:53:54 +0000

    enron is messed up...ohh and I fixed betty bugs

--- 0x03-more_functions_nested_loops/100-prime_factor.c
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- * _isdigit - checks if char is digit
- * @c: digit to be checked
+ * main - prints largest prime fac of n
  *
- * Description: checks if digit
+ * Description: largest prime of preset N
  * in a cool way
- * Return: 1 or 0
+ * Return: 0
  */
 int main(void)
 {
 	int i;
 	long n = 612852475143;
 
-    for (i = 2; i <= n; i++)
+	for (i = 2; i <= n; i++)
 	{
 		if (n % i == 0)
 		{
-			 n /= i;
-			 i--;
-		}				        
+			n /= i;
+			i--;
+		}
 	}
 	printf("%d\n", i);
 	return (0);
