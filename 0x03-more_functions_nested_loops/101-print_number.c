fb8708082f25dbc4afacbe0cc4fc8436fb3eaa60
Raid55 <boutboulnicholas@gmail.com>
2017-09-22 20:16:03 +0000

    101

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * print_number - prints n
  * @n: Number to be printed
@@ -9,31 +10,21 @@
  */
 void print_number(int n)
 {
-	int digit, rev;
-
+	int digit, rev = 0;
+	
+	if (n == 0)
+		_putchar('0');
 	if (n < 0)
 		_putchar('-'), n = n * -1;
-	if (n < 10)
+	while (n > 0)
 	{
-		_putchar(n + '0');
+		rev = rev * 10 + n % 10;
+		n /= 10;
 	}
-	else if (n < 100)
+	while (rev > 0)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else
-	{
-		while (n > 0)
-		{
-			rev = rev * 10 + n % 10;
-			n /= 10;
-		}
-		while (rev > 0)
-		{
-			digit = rev % 10;
-			_putchar(digit + '0');
-			rev /= 10;
-		}
+		digit = rev % 10;
+		_putchar(digit + '0');
+		rev /= 10;
 	}
 }
