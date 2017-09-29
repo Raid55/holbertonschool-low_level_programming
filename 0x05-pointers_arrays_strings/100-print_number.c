eed6016b85f3ef382fd10d2759f13696e9495ea9
Raid55 <boutboulnicholas@gmail.com>
2017-09-29 04:48:12 +0000

    alredy did print numbers and updated holberton.h

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_number - prints n
+ * @n: Number to be printed
+ *
+ * Description: prints n with putchar
+ * in a cool way
+ * Return: void
+ */
+void print_number(int n)
+{
+	int digit, rev = 0;
+	
+	if (n == 0)
+		_putchar('0');
+	if (n < 0)
+		_putchar('-'), n = n * -1;
+	while (n > 0)
+	{
+		rev = rev * 10 + n % 10;
+		n /= 10;
+	}
+	while (rev > 0)
+	{
+		digit = rev % 10;
+		_putchar(digit + '0');
+		rev /= 10;
+	}
+}
