136901a9483dd961fbb55fb3e7e3e0955a71adfd
Raid55 <boutboulnicholas@gmail.com>
2017-10-02 19:43:16 +0000

    first ex and boilerplate

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -11,11 +11,12 @@
 void print_number(int n)
 {
 	int digit, rev = 0;
-	
+	int N = n;
+
 	if (n == 0)
 		_putchar('0');
 	if (n < 0)
-		_putchar('-'), n = n * -1;
+		_putchar('-'), n = (n - 1) * -1;
 	while (n > 0)
 	{
 		rev = rev * 10 + n % 10;
@@ -24,6 +25,8 @@ void print_number(int n)
 	while (rev > 0)
 	{
 		digit = rev % 10;
+		if(N < 0 && rev < 10)
+			digit++;
 		_putchar(digit + '0');
 		rev /= 10;
 	}
---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _memset - changes nth amount of bytes with b starting from s
+ * @s:starting point
+ * @b:byte to change to
+ * @n: nth of bytes to chage
+ *
+ * Return: pointer to s 
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (n > 0)
+	{
+		s[i] = b;
+		i++, n--;
+	}
+	return (s);
+}
