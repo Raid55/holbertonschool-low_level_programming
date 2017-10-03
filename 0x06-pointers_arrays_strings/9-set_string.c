5d4d6e717430604a76592580b56fbd345b2021a5
Raid55 <boutboulnicholas@gmail.com>
2017-10-03 04:07:46 +0000

    mambo number 9, not as catchy as mambo numba 5

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - set s value to to
+ * @s: s's pointer to be changed
+ * @to: change pointer of s to to
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
