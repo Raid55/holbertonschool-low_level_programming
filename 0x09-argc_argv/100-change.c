41d03fd4921e364e7a5d90b5298023589f3434b0
Raid55 <boutboulnicholas@gmail.com>
2017-10-07 21:36:39 +0000

    i got change in my pocket

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - counts minimum coins needed for cents
+ * @argc: arg count
+ * @argv: arguments array
+ *
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int coins[5] = {25, 10, 5, 2, 1};
+	int num;
+	int totalCoins = 0;
+	int totalCents = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num = atoi(argv[1]);
+
+	while (totalCents != num)
+	{
+		totalCents += coins[i], totalCoins++;
+		if (totalCents > num)
+			totalCents -= coins[i++], totalCoins--;
+
+	}
+	printf("%d\n", totalCoins);
+	return (0);
+}
