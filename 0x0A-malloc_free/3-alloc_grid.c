05bd91e7528b53f81ddde1fd46ebce894fb6350d
Raid55 <boutboulnicholas@gmail.com>
2017-10-10 23:33:35 +0000

    added typecast and null test cases

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -29,7 +29,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	a = (void *)malloc(sizeof(int *) * height);
+	a = (int **)malloc(sizeof(int *) * height);
 	if (a == NULL)
 		return (NULL);
 	while (i < height)
