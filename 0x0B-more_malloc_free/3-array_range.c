a6dd8684f83f7fa96fa9503d94c324e6671c546d
Raid55 <boutboulnicholas@gmail.com>
2017-10-12 22:48:23 +0000

    fix

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -12,19 +12,18 @@
 int *array_range(int min, int max)
 {
 	int *a;
-	int i, I = 0;
+	int i = 0, I = 0;
+	int sum;
 
 	if (min > max)
 		return (NULL);
-	i = min;
-	while (max >= i)
-		I++, i++;
 
-	a = malloc(sizeof(int) * I);
+	a = malloc(sizeof(int) * (max - min) + 1);
 	if (a == NULL)
 		return (NULL);
-	while (min <= max)
-		a[i++] = min++;
+	I = min;
+	while (i <= (max - min) + 1)
+		a[i++] = I++;
 
 	return (a);
 }
