36966922b98f0db4a7acd2c33e681b09f0c6faea
Raid55 <boutboulnicholas@gmail.com>
2017-10-12 22:54:26 +0000

    test

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	sum = max - min;
-	a = malloc(sizeof(int) * sum + 1);
+	a = malloc(sizeof(int) * (sum + 1));
 	if (a == NULL)
 		return (NULL);
 	I = min;
