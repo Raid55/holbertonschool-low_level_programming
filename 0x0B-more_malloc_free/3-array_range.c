83bf1a7b3618c19d0907d045685277d9148addd5
Raid55 <boutboulnicholas@gmail.com>
2017-10-12 22:40:28 +0000

    I is now 0 bassed

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -6,19 +6,19 @@
  * array_range - makes an array with numbers
  * @min: start number
  * @max: max num
- * 
+ *
  * Return: pointer to arr or null
  */
 int *array_range(int min, int max)
 {
 	int *a;
-	int i = 0, I;
+	int i, I = 0;
 
 	if (min > max)
 		return (NULL);
-	I = min;
-	while (I <= max)
-		I++;
+	i = min;
+	while (max >= i)
+		I++, i++;
 
 	a = malloc(sizeof(int) * I);
 	if (a == NULL)
