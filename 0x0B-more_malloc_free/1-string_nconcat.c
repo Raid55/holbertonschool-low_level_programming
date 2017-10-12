5f3c7ce56b55d8c0af6f46bce9048527e2ba2de7
Raid55 <boutboulnicholas@gmail.com>
2017-10-12 21:48:21 +0000

    greater or equal to

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -7,25 +7,28 @@
  * @s1: string 1
  * @s2: string 2
  * @n: size to take from s2
+ *
+ * Return: poiter to new string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *a;
 	unsigned int s1Len, s2Len;
 	unsigned int i, I;
+	char *emptyString = "";
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = emptyString;
 	if (s2 == NULL)
-		s2 = ""; 
+		s2 = emptyString;
 
 	s1Len = strlen(s1);
 	s2Len = strlen(s2);
 
-	a = malloc(s1Len + 1 + n > s2Len ? s2Len : n );
+	a = malloc(s1Len + 1 + n >= s2Len ? s2Len : n);
 	if (a == NULL)
 		return (NULL);
-	
+
 	i = 0, I = 0;
 	while (s1[i] != '\0')
 		a[I++] = s1[i++];
