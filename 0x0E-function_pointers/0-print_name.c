678662782383f9be44bbbb6a5db3595ac79c1eda
Raid55 <boutboulnicholas@gmail.com>
2017-10-18 10:13:15 +0000

    late night pointer functions with sid

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints name with function
+ * @name: name
+ * @f: function formating
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
