026ff214778cc24fdfdcdd86177dda125033a901
Raid55 <boutboulnicholas@gmail.com>
2017-10-18 19:59:58 +0000

    init after argc check

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -14,12 +14,13 @@ int main(int argc, char *argv[])
 	int (*final_operator)(int, int);
 	int num1;
 	int num2;
+	
+	
+	if (argc != 4)
+		printf("Error\n"), exit(98);
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-
-	if (argc != 4)
-		printf("Error\n"), exit(98);
 
 	if ((argv[2][0] == '/' && num2 == 0) ||
 			(argv[2][0] == '%' && num2 == 0))
