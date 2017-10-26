f06cd865dbf19762c6f4e59cccb35db70bbf32f2
Raid55 <boutboulnicholas@gmail.com>
2017-10-26 19:58:17 +0000

    100-fast, learned what a constructor is

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * _startFunc - function that has the constructor attribute
+ *
+ * Return: void
+ */
+void _startFunc(void)
+{
+	printf(
+	"You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
