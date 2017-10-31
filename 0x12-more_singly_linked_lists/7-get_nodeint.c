6b0e113875029736358cfbc4f1a91dd3680ead43
Nicholas Boutboul <boutboulnicholas@gmail.com>
2017-10-31 03:25:44 -0400

    added NULL check and return val

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 
 	while (i < index)
+	{
+		if (!head)
+			return (NULL);
 		head = head->next, i++;
+	}
 
 	return (head);
 }
