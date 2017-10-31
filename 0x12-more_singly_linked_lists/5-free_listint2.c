e522ae4fb8ae8f310794961c26d0180d80470569
Nicholas Boutboul <boutboulnicholas@gmail.com>
2017-10-31 04:07:35 -0400

    betty and change dereferance logic to change head instead of tmp

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -14,10 +14,9 @@ void free_listint2(listint_t **head)
 
 	while (*head != NULL)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		tmp = (*head)
+		free(tmp);
+		*head = (*head)->next;
 	}
-	
 	*head = NULL;
 }
