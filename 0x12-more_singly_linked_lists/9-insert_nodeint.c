5daa0db2d814d145891e134f1f9f96c54181fce7
Raid55 <boutboulnicholas@gmail.com>
2017-10-31 08:36:35 +0000

    freeing when function crashes

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,12 +17,30 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	if (*head == NULL)
+	{
+		free(newNode);
 		return (NULL);
+	}
+
+	if (idx == 0)
+	{
+		newNode->n = n;
+		if ((*head)->next != NULL)
+			newNode->next = (*head)->next;
+		else
+			newNode->next = NULL;
+		*head = newNode;
+		return (newNode);
+	}
+
 	tmp = *head;
 	while (tmp && i < (idx - 1))
 		tmp = tmp->next, i++;
 	if (!tmp)
+	{
+		free(newNode);
 		return (NULL);
+	}
 
 	newNode->n = n;
 
