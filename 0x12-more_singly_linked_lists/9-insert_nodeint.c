2db5b5428e3a3a894a3338fb9b9390a9a90598d7
Raid55 <boutboulnicholas@gmail.com>
2017-11-01 02:48:23 +0000

    reworked 9 with help

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,41 +11,36 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *newNode = malloc(sizeof(listint_t));
 	listint_t *tmp;
-	unsigned int i = 0;
+	unsigned int i = 1;
 
 	if (!newNode)
 		return (NULL);
 
-	if (*head == NULL)
-	{
-		free(newNode);
-		return (NULL);
-	}
-
-	if (idx == 0)
-	{
-		newNode->n = n;
-		if ((*head)->next != NULL)
-			newNode->next = (*head)->next;
-		else
-			newNode->next = NULL;
-		*head = newNode;
-		return (newNode);
-	}
-
-	tmp = *head;
-	while (tmp && i < (idx - 1))
-		tmp = tmp->next, i++;
-	if (!tmp)
+	if (head == NULL)
 	{
 		free(newNode);
 		return (NULL);
 	}
 
 	newNode->n = n;
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
 
-	newNode->next = tmp->next;
-	tmp->next = newNode;
-
-	return (newNode);
+	tmp = *head;
+	while (tmp)
+	{
+		if (i == idx)
+		{
+			newNode->next = tmp->next;
+			tmp->next = newNode;
+			return (newNode);
+		}
+		tmp = tmp->next, i++;
+	}
+	free(newNode);
+	return (NULL);
 }
