ac7f2fd964914cfe99922f7c11b78bdd3a481b72
Raid55 <boutboulnicholas@gmail.com>
2017-11-02 23:24:32 +0000

    reworked func thanks to sid and added helper func to fix 0 issue

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,19 +1,34 @@
 #include "holberton.h"
 /**
- * print_binary - prints binary with putchar
+ * print_binary_helper - prints binary w/putchar that isent 0, helps main func
  * @n: number to print in binary
+ *
+ * Return: VOID
+ */
+void print_binary_helper(unsigned long int n)
+{
+	if (n != 0)
+	{
+		if (n & 1)
+			n = n >> 1, print_binary_helper(n), _putchar('1');
+		else
+			n = n >> 1, print_binary_helper(n), _putchar('0');
+	}
+	else
+	{
+		return;
+	}
+}
+/**
+ * print_binary - checks for 0 or sends of work to helper func
+ * @n: num to change to bin
  *
  * Return: VOID
  */
 void print_binary(unsigned long int n)
 {
-	if (n <= 1) 
-	{
-		_putchar('0' + n);
-	}
-    else
-	{
-		print_binary(n / 2);
-		_putchar('0' + n % 2);
-	}
+	if (n == 0)
+		_putchar('0');
+	else
+		print_binary_helper(n);
 }
