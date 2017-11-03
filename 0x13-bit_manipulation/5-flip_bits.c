5759d2b064e133eb37fa752fd2e43edecad1fb0c
Raid55 <boutboulnicholas@gmail.com>
2017-11-03 01:30:14 +0000

    finished working on 5, first betty test

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * flip_bits - finds how much bits need to be fliped for n and m to be same
+ * @n: first num to compaire
+ * @m: second num to comaire
+ *
+ * Return: int of difference between nums
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int sum = 0;
+	unsigned long int tmp;
+	unsigned int i = 0;
+
+	tmp = n ^ m;
+
+	while (i <= sizeof(unsigned long int) * 4)
+	{
+		sum += (tmp >> i) & 1;
+		i++;
+	}
+	return (sum);
+}
