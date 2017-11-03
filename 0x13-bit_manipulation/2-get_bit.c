d12cba91818eeb6de8ca7b676ef33cf1c19eccb3
Raid55 <boutboulnicholas@gmail.com>
2017-11-03 00:53:40 +0000

    changed logic and trying with helper func

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,27 +1,45 @@
 #include "holberton.h"
 /**
- * get_bit - gets bit at certain index
- * @n: int to turn into base 2
- * @index: index what bit to extract
+ * get_bit_helper - helps the function with all of the logic
+ * @n: number to extract bit from
+ * @index: index pos to extract bit info
  *
- * Return: int at bit pos
+ * Return: 1 or 0 depending one what byte or -1 on error
+ */
+int get_bit_helper(unsigned long int n, unsigned int index)
+{
+	if (n != 0)
+	{
+		if (n & 1)
+		{
+			n = n >> 1;
+			if (index == 0)
+				return (1);
+			return (get_bit_helper(n, index - 1));
+		}
+		else
+		{
+			n = n >> 1;
+			if (index == 0)
+				return (0);
+			return (get_bit_helper(n, index - 1));
+		}
+	}
+	return (-1);
+}
+/**
+ * get_bit - starts the get bit func, uses same logic as last ex
+ * @n: num to analize for bit at index
+ * @index: pos to extract bit
+ *
+ * Return: 1 ro 0 depending on which bit or -1 if error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > 64)
+	if (index > 32)
 		return (-1);
-
-	if (n <= 1)
-	{
-		if (!index)
-			return (n);
-		else
-			return (-1);
-	}
+	if (n == 0)
+		return (0);
 	else
-	{
-		if (!index)
-			return (n % 2);
-		return (get_bit(n / 2, index - 1));
-	}
+		return (get_bit_helper(n, index));
 }
