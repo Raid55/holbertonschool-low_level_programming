e5c446e5de258675022d5944ded81833eb2295a4
Raid55 <boutboulnicholas@gmail.com>
2017-11-07 20:03:35 +0000

    added free and malloc and doc

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,10 +1,19 @@
 #include "holberton.h"
-
+/**
+ * read_textfile - reads a file and prints to buffer
+ * @filename: name of file
+ * @letters: amount of chars to be printed
+ *
+ * Return: number of chars printed
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
 	int tmp;
-	char *buff[2048];
+	char *buff = malloc(sizeof(char) * letters + 1);
+
+	if (buff == NULL)
+		return (0);
 
 	if (filename == NULL)
 		return (0);
@@ -13,13 +22,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (file == -1)
 		return (0);
-	
+
 	tmp = read(file, buff, letters);
 	buff[letters] = '\0';
 
 	write(1, buff, letters);
 
 	close(file);
+	free(buff);
 
 	return (tmp);
 }
--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -3,6 +3,7 @@
 
 /*standard libs*/
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
