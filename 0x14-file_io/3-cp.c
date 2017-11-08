729be37705d9430d1337987dae6100a46552189a
Raid55 <boutboulnicholas@gmail.com>
2017-11-08 02:01:48 +0000

    added descriptors

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -9,15 +9,15 @@ int main(int ac, char **av)
 	char buff[1025];
 
 	if (ac != 3)
-		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	oFile = open(av[1], O_RDONLY);
 	if (oFile == -1)
-		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	
 	nFile = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (nFile == -1)
-		dprintf(2, "Error: Can't write to %s\n", av[2]), exit (99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit (99);
 
 	while (rErr != 0)
 	{
@@ -25,15 +25,15 @@ int main(int ac, char **av)
 		if (rErr != 0)
 			wErr = write(nFile, buff, rErr);
 		if (wErr == -1)
-			dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 	
 	err = close(oFile);
 	if (err == -1)
-		dprintf(2, "Error: Can't close fd %i\n", oFile), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", oFile), exit(100);
 	err = close(nFile);
 	if (err == -1)
-		dprintf(2, "Error: Can't close fd %i\n", nFile), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", nFile), exit(100);
 
-	return (1);
+	return (0);
 }
