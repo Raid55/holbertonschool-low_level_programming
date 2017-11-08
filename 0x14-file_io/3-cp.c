ee0347ddfbef0c9323d0cd04a5cc2aaf5f28e62b
Raid55 <boutboulnicholas@gmail.com>
2017-11-08 05:24:21 +0000

    refractored and check for arg if null

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -16,32 +16,59 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	if (!av[1])
+		__read_err(av[1]);
+	if (!av[2])
+		__write_err(av[2]);
 
 	oFile = open(av[1], O_RDONLY);
 	if (oFile == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		__read_err(av[1]);
 
 	nFile = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (nFile == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		__write_err(av[2]);
 
 	while (rErr != 0)
 	{
 		rErr = read(oFile, buff, 1024);
 		if (rErr == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+			__read_err(av[1]);
 		if (rErr != 0)
 			wErr = write(nFile, buff, rErr);
 		if (wErr == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+			__write_err(av[2]);
 	}
 
 	err = close(oFile);
 	if (err == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", oFile), exit(100);
+		__close_err(oFile);
 	err = close(nFile);
 	if (err == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", nFile), exit(100);
-
+		__close_err(nFile);
 	return (0);
+}
+/**
+ * __read_err - read error and exit
+ * @s: error file
+ */
+void __read_err(char *s)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s), exit(98);
+}
+/**
+ * __write_err - writes error and exits
+ * @s: error file
+ */
+void __write_err(char *s)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s), exit(99);
+}
+/**
+ * __close_err - error on file close and exits
+ * @d: descriptor
+ */
+void __close_err(int d)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", d), exit(100);
 }
--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -14,5 +14,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+/*cp error prototypes*/
+void __write_err(char *);
+void __read_err(char *);
+void __close_err(int);
 
 #endif
