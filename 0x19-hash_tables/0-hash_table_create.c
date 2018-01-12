00d77760880db660e7d9cebafc645e997fbd59fc
Nicholas Boutboul <boutboulnicholas@gmail.com>
2018-01-12 14:30:21 -0800

    lol python formating

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -5,11 +5,11 @@ hash_table_t *hash_table_create(unsigned long int size)
     hash_table_t *table = NULL;
 
     if (size == 0)
-        return NULL;
+        return (NULL);
 
     table = malloc(sizeof(hash_table_t));
     if (table == NULL)
-        return NULL;
+        return (NULL);
 
     table->size = size;
     table->array = calloc(size, sizeof(hash_node_t *));
