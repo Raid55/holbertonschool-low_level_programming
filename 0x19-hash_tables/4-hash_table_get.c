8d55e4b27eeb6db514c4f74618a0a677ad3d2e35
Raid55 <boutboulnicholas@gmail.com>
2018-01-13 00:42:31 +0000

    is betty the solution to all my probs

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -1,16 +1,22 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_get - gets value for hash table wit tha value
+ * @ht: hash table
+ * @key: key to lookup
+ *
+ * Return: returns value or null
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *tmp;
 	unsigned long int keyInd;
-	
+
 	if (ht == NULL || key == NULL)
 		return (NULL);
 
 	keyInd = key_index((const unsigned char *)key, ht->size);
 
-	tmp = ht->array[keyInd];	
+	tmp = ht->array[keyInd];
 	while (tmp)
 	{
 		if (!strcmp(tmp->key, key))
