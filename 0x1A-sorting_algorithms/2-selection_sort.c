090d59612012dd8cc967ab15208ebf72299f13be
Raid55 <boutboulnicholas@gmail.com>
2018-01-30 20:32:42 +0000

    selection sort without c99 comments

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -1,28 +1,33 @@
 #include "sort.h"
 
-
+/**
+ * selection_sort - selection sort algo
+ * @array: array to be sorted
+ * @size: size of array to sort
+ *
+ * Return: sorted array
+ */
 void selection_sort(int *array, size_t size)
 {
 	size_t i = 0, j, k;
 	int tmp;
- 
-    while (i < size - 1)
-    {
-        k = i, j = i + 1;
-        while (j < size)
+
+	while (i < size - 1)
+	{
+		k = i, j = i + 1;
+		while (j < size)
 		{
 			if (array[j] < array[k])
 				k = j;
 			j++;
 		}
-
-		//Swap 
+		/*Swaping action*/
 		tmp = array[k];
 		array[k] = array[i];
 		array[i] = tmp;
 
-		//Print
+		/*Print and Incr*/
 		print_array(array, size);
 		i++;
-    }
+	}
 }
