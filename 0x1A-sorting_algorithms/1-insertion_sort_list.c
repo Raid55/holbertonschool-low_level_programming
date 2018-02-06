711cf91eb5150a50e3beee68b4247824c278dc5b
Raid55 <boutboulnicholas@gmail.com>
2018-02-06 17:18:49 +0000

    small fix

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -7,7 +7,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr;
 
-	if (!(*list) || !list || !(*list)->next)
+	if (!list || !(*list) || !(*list)->next)
 		return;
 
 	curr = (*list);
