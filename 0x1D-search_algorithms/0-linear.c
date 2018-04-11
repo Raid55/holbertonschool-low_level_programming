a6e0171f1a514b9e5c078299058f4866d9c0e309
Raid55 <boutboulnicholas@gmail.com>
2018-04-11 23:56:18 +0000

    typecast

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -10,14 +10,14 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	int i = 0;
 
 	if (array == NULL)
 		return (-1);
 
-	while (i < size)
+	while (i < (int)size)
 	{
-		printf("Value checked array[%zu] = [%d]\n", i, array[i]);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 		i++;
